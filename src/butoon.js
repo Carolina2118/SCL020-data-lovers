0a0c89308ad6571831b30cce463bc4b364ecba5a
caro <loboscarolinaa@gmail.com>
2022-05-24 11:30:01 -0400

    se agregó movimiento al boton

---
+++ src/butoon.js
@@ -0,0 +1,6 @@
+const navButon = document.querySelector(".nav-buton")
+const navMenu = document.querySelector(".nav-menu")
+navButon.addEventListener("click", () => {
+    navMenu.classList.toggle("nav-menu_visible")
+        //devuelve los nombres de clase CSS de un elemento.
+});
