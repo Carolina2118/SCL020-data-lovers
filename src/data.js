2779239dbc9975539003831a72dd58e5ba1b0fa3
caro <loboscarolinaa@gmail.com>
2022-06-07 18:11:11 -0400

    agregando interpolacion de cadenas

--- src/data.js
+++ src/data.js
@@ -10,17 +10,12 @@
  //**************------------****************//
  /*es para que recorra el array uno a uno y muestre el elemento */
 
- export function recorrePersonaje(dataRecorrer) {
-     for (let i = 0; i < dataRecorrer.length; i++) {
-         const namePersonajes = dataRecorrer[i].name;
-         console.log(namePersonajes);
-         //return namePersonajes;
-     }
- };
+
  export function recorregenero(dataRecorrerGenero) {
      for (let i = 0; i < dataRecorrerGenero.length; i++) {
          const recorregenero = dataRecorrerGenero[i].gender;
+
+
          console.log(recorregenero);
-         //return namePersonajes;
      }
  };
--- src/main.js
+++ src/main.js
@@ -1,29 +1,34 @@
-import { filterDatos, recorrePersonaje, recorregenero } from './data.js';
+import { filterDatos, recorregenero } from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
 let rickDatos = data.results;
 console.log(rickDatos);
 /*Diseño del afiche e imagen */
-const namePersonajes = (personajes) => {
+const mostrarPersonajes = (personajes) => {
     personajes.forEach(e => {
         let perName = e.name
         let geMaleFemal = e.gender
+        let imagen = e.image
         document.getElementById("root").innerHTML += `
-   <h2 class="personaje">Personaje:${perName}</h2>
-   <h3 class ="personaje">Genero:${geMaleFemal}</h3>
+   <h1 class="personaje">Personaje:${perName}</h1>
+   <h2 class ="genero">Genero:${geMaleFemal}</h2>
+   <img src ="${imagen}">
      `
     })
 }
 
-namePersonajes(rickDatos);
+//namePersonajes(rickDatos);
 
 /*la funciond el click en personaje*/
 /****** funciona! *****/
 const personajesName = document.getElementById("personajes");
-personajesName.addEventListener("click", function(e) {
-    recorrePersonaje(rickDatos);
+personajesName.addEventListener("click", function() {
+    //recorrePersonaje(rickDatos);
+    mostrarPersonajes(rickDatos);
 });
 const generoMaleFemal = document.getElementById("genero");
 generoMaleFemal.addEventListener("click", function(e) {
-    recorregenero(rickDatos);
+
+
+    // mostrarPersonajes(rickDatos);
 });
