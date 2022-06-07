34653e6e27558377561905ff85c026ff4203423c
caro <loboscarolinaa@gmail.com>
2022-06-06 22:19:00 -0400

    recorriendo el array con genero

--- src/data.js
+++ src/data.js
@@ -10,10 +10,17 @@
  //**************------------****************//
  /*es para que recorra el array uno a uno y muestre el elemento */
 
- export function rick(dataRecorrer) {
+ export function recorrePersonaje(dataRecorrer) {
      for (let i = 0; i < dataRecorrer.length; i++) {
          const namePersonajes = dataRecorrer[i].name;
          console.log(namePersonajes);
+         //return namePersonajes;
+     }
+ };
+ export function recorregenero(dataRecorrerGenero) {
+     for (let i = 0; i < dataRecorrerGenero.length; i++) {
+         const recorregenero = dataRecorrerGenero[i].gender;
+         console.log(recorregenero);
          //return namePersonajes;
      }
  };
--- src/main.js
+++ src/main.js
@@ -1,14 +1,29 @@
-import { filterDatos, rick } from './data.js';
+import { filterDatos, recorrePersonaje, recorregenero } from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
 let rickDatos = data.results;
 console.log(rickDatos);
+/*Diseño del afiche e imagen */
+const namePersonajes = (personajes) => {
+    personajes.forEach(e => {
+        let perName = e.name
+        let geMaleFemal = e.gender
+        document.getElementById("root").innerHTML += `
+   <h2 class="personaje">Personaje:${perName}</h2>
+   <h3 class ="personaje">Genero:${geMaleFemal}</h3>
+     `
+    })
+}
 
+namePersonajes(rickDatos);
 
+/*la funciond el click en personaje*/
+/****** funciona! *****/
 const personajesName = document.getElementById("personajes");
 personajesName.addEventListener("click", function(e) {
-    // e.preventDefault();
-    console.log("hola");
-
-    rick(rickDatos);
+    recorrePersonaje(rickDatos);
+});
+const generoMaleFemal = document.getElementById("genero");
+generoMaleFemal.addEventListener("click", function(e) {
+    recorregenero(rickDatos);
 });
